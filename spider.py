7ef6c0a7c780d4289454ffbf37c031900b04482b
kkyyGGYY <767298436@qq.com>
2017-10-01 12:58:11 +0800

    change

--- spider.py
+++ spider.py
@@ -45,8 +45,8 @@ def main(offset):
 
 
 if __name__ == '__main__':
-    for i in range(10):
-
-        main(i * 10)
-    # pool = Pool()
-    # pool = pool.map(main, [i * 10 for i in range(10)])
+    # for i in range(10):
+    #
+    #     main(i * 10)
+    pool = Pool()
+    pool = pool.map(main, [i * 10 for i in range(10)])
